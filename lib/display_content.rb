2a5eb5677354078b90e78fd215d2719208968abe
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2020-11-16 14:59:22 +0200

    Added working gem display_content

--- lib/display_content.rb
+++ lib/display_content.rb
@@ -1,6 +1,28 @@
 require "display_content/version"
+require "erb"
 
 module DisplayContent
   class Error < StandardError; end
-  # Your code goes here...
+  def self.display_content(content, bypass_html = false, file_name = "index")
+    bypass_html ? content : content.gsub!(%r{<([^>]+?)([^>]*?)>(.*?)</\1>}, "")
+    output_file = File.expand_path("#{file_name}.html")
+    template = %(<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Content to HTML</title>
+  </head>
+  <body>
+    <%= @content %>
+  </body>
+  </html>
+  )
+    @content = content
+    renderer = ERB.new(template)
+    result = renderer.result(binding)
+    File.open(output_file, "w") do |f|
+      f.write(result)
+      p "DisplayContent done!"
+    end
+  end
 end
